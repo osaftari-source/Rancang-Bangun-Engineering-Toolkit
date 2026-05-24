c2842cfce084d659900c10ebf4225eae313c98f2
osaftari-source <osaftari@gmail.com>
2026-05-24 20:07:14 +0700

    Delete assets directory

--- assets/js/about.js
+++
@@ -1,7 +0,0 @@
-/* About page support — Rancang Bangun Engineering Toolkit Hub v1.1 */
-(() => {
-  "use strict";
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("./sw.js?v=1.1.0", { updateViaCache: "none" }).catch(() => {});
-  }
-})();
